88a417891c7bbcf53b9649b6321a530831a674fe
klaatuka <klaatuka82@GMAIL.COM>
2022-11-20 23:44:04 +0300

    1

--- ColorView/ViewController.swift
+++ ColorView/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.minimumValue = 1
+        redSlider.maximumValue = 255
+        greenSlider.minimumValue = 1
+        greenSlider.maximumValue = 255
+        blueSlider.minimumValue = 1
+        blueSlider.maximumValue = 255
+        
     }
-
-
+    
+    @IBAction func redAction(_ sender: UISlider) {
+        redSlider.minimumTrackTintColor = .red
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: 1)
+    }
+    
+    @IBAction func greenSlider(_ sender: UISlider) {
+        greenSlider.minimumTrackTintColor = .green
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: 1)
+    }
+    
+    @IBAction func blueAction(_ sender: UISlider) {
+        blueSlider.minimumTrackTintColor = .blue
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: 1)
+    }
+    
 }
 
