1c2b7a6cfe3b736562cb7d6605cce0552b24b57e
klaatuka <klaatuka82@GMAIL.COM>
2022-11-21 00:14:03 +0300

    finish

--- ColorView/ViewController.swift
+++ ColorView/ViewController.swift
@@ -29,22 +29,32 @@ class ViewController: UIViewController {
         blueSlider.minimumValue = 1
         blueSlider.maximumValue = 255
         
+        mainView.backgroundColor = .black
+        
+        view.backgroundColor = UIColor(red: 1 / 255, green: 59 / 255, blue: 255 / 255, alpha: 0.8)
+        
     }
     
     @IBAction func redAction(_ sender: UISlider) {
         redSlider.minimumTrackTintColor = .red
-        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: 1)
+        draftview()
+        redLabel.text = String(Int(redSlider.value))
     }
     
     @IBAction func greenSlider(_ sender: UISlider) {
         greenSlider.minimumTrackTintColor = .green
-        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: 1)
+        draftview()
+        greenLabel.text = String(Int(greenSlider.value))
     }
     
     @IBAction func blueAction(_ sender: UISlider) {
         blueSlider.minimumTrackTintColor = .blue
-        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: 1)
+       draftview()
+        blueLabel.text = String(Int(blueSlider.value))
     }
     
+    func draftview(){
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: 1)
+    }
 }
 
